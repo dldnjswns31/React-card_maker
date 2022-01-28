61c52a983cdbd9333d665bca4943b9eeb93d1650
dldnjswns31 <dldnjswns32@gmail.com>
2022-01-28 16:37:01 +0900

    add maker and redirect with react-router

---
+++ src/components/maker/maker.jsx
@@ -0,0 +1,31 @@
+import React, { useEffect } from "react";
+import { useLocation, useNavigate } from "react-router-dom";
+import Footer from "../footer/footer";
+import Header from "../header/header";
+import styles from "./maker.module.css";
+
+const Maker = ({ authService }) => {
+  const navigate = useNavigate();
+  const location = useLocation();
+  console.log(location);
+
+  const onLogout = () => {
+    authService.logout();
+  };
+  useEffect(() => {
+    authService.onAuthChange((user) => {
+      if (!user) {
+        navigate("/");
+      }
+    });
+  });
+
+  return (
+    <section className={styles.maker}>
+      <Header onLogout={onLogout} />
+      <Footer />
+    </section>
+  );
+};
+
+export default Maker;
